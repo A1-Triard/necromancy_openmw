e3cd7d375160f34cea74dd98e62f902f1990578c
Master <internalmike@gmail.com>
2026-06-30 04:00:59 +0300

    activators

--- A1_Necromancy_Heads/src/main.rs
+++ A1_Necromancy_Heads/src/main.rs
@@ -1,8 +1,10 @@
+#![deny(warnings)]
+
 use either::Right;
 use esl::{Field, BodyPartKind, BodyPartType, NAME, BODY, BYDT, MODL, CodePage};
 use esl::{Record, TES3, HEDR, RecordFlags, FileMetadata, FileType, StringZ, FNAM};
 use esl::{BodyPart, BodyPartFlags, ARMO, AODT, Armor, ArmorType, ITEX, INDX, BipedObject};
-use esl::{BNAM, MISC, MCDT, MiscItem, RecordSerde};
+use esl::{BNAM, MISC, MCDT, MiscItem, RecordSerde, SCRI, ACTI};
 use esl::code::{self};
 use esl::read::{Records, RecordReadMode};
 use serde_serialize_seed::{ValueWithSeed, VecSerde};
@@ -76,10 +78,15 @@ fn find_openmw_cfg() -> Option<PathBuf> {
     Some(exe_dir.join("openmw.cfg"))
 }
 
+struct RaceAndModel {
+    race: String,
+    model: String,
+}
+
 fn collect_heads_and_hairs(
     path: &Path,
-    heads: &mut HashMap<String, String>,
-    hairs: &mut HashMap<String, String>,
+    heads: &mut HashMap<String, RaceAndModel>,
+    hairs: &mut HashMap<String, RaceAndModel>,
 ) {
     let mut file = match File::open(path) {
         Ok(file) => file,
@@ -104,15 +111,21 @@ fn collect_heads_and_hairs(
             eprintln!("Missing MODL field in {}", path.display());
             continue;
         };
+        let Some(fnam_field) = record.fields.iter().find(|(tag, _)| *tag == FNAM) else {
+            eprintln!("Missing FNAM field in {}", path.display());
+            continue;
+        };
         let Field::BodyPart(part) = &bydt_field.1 else { panic!() };
         if part.body_part_type != BodyPartType::Skin { continue; }
         let Field::StringZ(id) = &name_field.1 else { panic!() };
         let id = id.string.to_lowercase();
         let Field::StringZ(model) = &modl_field.1 else { panic!() };
         let model = model.string.clone();
+        let Field::StringZ(race) = &fnam_field.1 else { panic!() };
+        let race = race.string.clone();
         match part.kind {
-            BodyPartKind::Head => { heads.insert(id, model); },
-            BodyPartKind::Hair => { hairs.insert(id, model); },
+            BodyPartKind::Head => { heads.insert(id, RaceAndModel { race, model }); },
+            BodyPartKind::Hair => { hairs.insert(id, RaceAndModel { race, model }); },
             _ => { },
         }
     }
@@ -126,10 +139,42 @@ fn id_hash(id: &str) -> u32 {
     res % 100000000
 }
 
+fn race_name(race: &str) -> &'static str {
+    match race {
+        "High Elf" => "высокого эльфа",
+        "Argonian" => "аргонианина",
+        "Wood Elf" => "лесного эльфа",
+        "Breton" => "бретона",
+        "Dark Elf" => "темного эльфа",
+        "Imperial" => "имперца",
+        "Khajiit" => "хаджита",
+        "Nord" => "нордлинга",
+        "Orc" => "орка",
+        "Redguard" => "редгарда",
+        _ => "",
+    }
+}
+
+fn head_script(race: &str) -> &'static str {
+    match race {
+        "High Elf" => "A1_NecroAltmerHeadSc",
+        "Argonian" => "A1_NecroArgHeadSc",
+        "Wood Elf" => "A1_NecroBosmerHeadSc",
+        "Breton" => "A1_NecroBretonHeadSc",
+        "Dark Elf" => "A1_NecroDunmerHeadSc",
+        "Imperial" => "A1_NecroImpHeadSc",
+        "Khajiit" => "A1_NecroKhaHeadSc",
+        "Nord" => "A1_NecroNordHeadSc",
+        "Orc" => "A1_NecroOrcHeadSc",
+        "Redguard" => "A1_NecroRgHeadSc",
+        _ => "",
+    }
+}
+
 fn write_esp(
     path: &Path,
-    heads: &HashMap<String, String>,
-    hairs: &HashMap<String, String>,
+    heads: &HashMap<String, RaceAndModel>,
+    hairs: &HashMap<String, RaceAndModel>,
 ) {
     let mut records = Vec::new();
     records.push(Record {
@@ -139,8 +184,8 @@ fn write_esp(
             (HEDR, Field::FileMetadata(FileMetadata {
                 version: 1067869798,
                 file_type: FileType::ESP,
-                author: Right("potions_balance".to_string()),
-                description: Right(vec!["Potions balance.".into()]),
+                author: Right("A1".to_string()),
+                description: Right(vec!["Головы для A1_Necromancy.omwscripts".into()]),
                 records:
                     u32::try_from(hairs.len()).unwrap_or(u32::MAX)
                         .saturating_mul(2)
@@ -148,14 +193,14 @@ fn write_esp(
             }))
         ],
     });
-    for (hair_id, hair_model) in hairs {
+    for (hair_id, hair) in hairs {
         let hash = id_hash(hair_id);
         let body_record = Record {
             tag: BODY,
             flags: RecordFlags::empty(),
             fields: vec![
                 (NAME, Field::StringZ(StringZ { string: format!("A1_NecroB{hash}"), has_tail_zero: true })),
-                (MODL, Field::StringZ(StringZ { string: hair_model.clone(), has_tail_zero: true })),
+                (MODL, Field::StringZ(StringZ { string: hair.model.clone(), has_tail_zero: true })),
                 (FNAM, Field::StringZ(StringZ { string: "Breton".to_string(), has_tail_zero: true })),
                 (BYDT, Field::BodyPart(BodyPart {
                     kind: BodyPartKind::Hair,
@@ -167,15 +212,17 @@ fn write_esp(
         };
         records.push(body_record);
     }
-    for (hair_id, hair_model) in hairs {
+    for (hair_id, hair) in hairs {
+        let race = race_name(&hair.race);
+        if race.is_empty() { continue; }
         let hash = id_hash(hair_id);
         let armo_record = Record {
             tag: ARMO,
             flags: RecordFlags::empty(),
             fields: vec![
                 (NAME, Field::StringZ(StringZ { string: format!("A1_NecroH{hash}"), has_tail_zero: true })),
-                (MODL, Field::StringZ(StringZ { string: hair_model.clone(), has_tail_zero: true })),
-                (FNAM, Field::StringZ(StringZ { string: "Скальп".to_string(), has_tail_zero: true })),
+                (MODL, Field::StringZ(StringZ { string: hair.model.clone(), has_tail_zero: true })),
+                (FNAM, Field::StringZ(StringZ { string: format!("Скальп {race}"), has_tail_zero: true })),
                 (AODT, Field::Armor(Armor {
                     armor_type: ArmorType::Helmet,
                     weight: 0.2,
@@ -193,15 +240,17 @@ fn write_esp(
         };
         records.push(armo_record);
     }
-    for (head_id, head_model) in heads {
+    for (head_id, head) in heads {
+        let race = race_name(&head.race);
+        if race.is_empty() { continue; }
         let hash = id_hash(head_id);
         let misc_record = Record {
             tag: MISC,
             flags: RecordFlags::empty(),
             fields: vec![
                 (NAME, Field::StringZ(StringZ { string: format!("A1_NecroH{hash}"), has_tail_zero: true })),
-                (MODL, Field::StringZ(StringZ { string: head_model.clone(), has_tail_zero: true })),
-                (FNAM, Field::StringZ(StringZ { string: "Голова".to_string(), has_tail_zero: true })),
+                (MODL, Field::StringZ(StringZ { string: head.model.clone(), has_tail_zero: true })),
+                (FNAM, Field::StringZ(StringZ { string: format!("Голова {race}"), has_tail_zero: true })),
                 (MCDT, Field::MiscItem(MiscItem {
                     weight: 1.0,
                     value: 0,
@@ -213,6 +262,23 @@ fn write_esp(
             ],
         };
         records.push(misc_record);
+    }
+    for (head_id, head) in heads {
+        let race = race_name(&head.race);
+        if race.is_empty() { continue; }
+        let script = head_script(&head.race);
+        let hash = id_hash(head_id);
+        let acti_record = Record {
+            tag: ACTI,
+            flags: RecordFlags::empty(),
+            fields: vec![
+                (NAME, Field::StringZ(StringZ { string: format!("A1_NecroX{hash}"), has_tail_zero: true })),
+                (MODL, Field::StringZ(StringZ { string: head.model.clone(), has_tail_zero: true })),
+                (FNAM, Field::StringZ(StringZ { string: format!("Голова {race}"), has_tail_zero: true })),
+                (SCRI, Field::StringZ(StringZ { string: script.to_string(), has_tail_zero: true })),
+            ],
+        };
+        records.push(acti_record);
     }
     {
         let file = match File::create(path) {
--- Data Files/Scripts/A1_Necromancy/global.lua
+++ Data Files/Scripts/A1_Necromancy/global.lua
@@ -13,6 +13,45 @@ end
 
 local heads = { }
 
+local pauls = {
+    ['high elf'] = 'a1_necroaltmerpaul',
+    ['argonian'] = 'a1_necroargpaul',
+    ['wood elf'] = 'a1_necrobosmerpaul',
+    ['breton'] = 'a1_necrobretonpaul',
+    ['dark elf'] = 'a1_necrodunmerpaul',
+    ['imperial'] = 'a1_necroimppaul',
+    ['khajiit'] = 'a1_necrokhapaul',
+    ['nord'] = 'a1_necronordpaul',
+    ['orc'] = 'a1_necroorcpaul',
+    ['redguard'] = 'a1_necrorgpaul',
+}
+
+local bodies = {
+    ['a1_necroaltmerpaul'] = 'a1_necroaltmerbody',
+    ['a1_necroargpaul'] = 'a1_necroargbody',
+    ['a1_necrobosmerpaul'] = 'a1_necrobosmerbody',
+    ['a1_necrobretonpaul'] = 'a1_necrobretonbody',
+    ['a1_necrodunmerpaul'] = 'a1_necrodunmerbody',
+    ['a1_necroimppaul'] = 'a1_necroimpbody',
+    ['a1_necrokhapaul'] = 'a1_necrokhabody',
+    ['a1_necronordpaul'] = 'a1_necronordbody',
+    ['a1_necroorcpaul'] = 'a1_necroorcbody',
+    ['a1_necrorgpaul'] = 'a1_necrorgbody',
+}
+
+local paulsByBodies = {
+    ['a1_necroaltmerbody'] = 'a1_necroaltmerpaul',
+    ['a1_necroargbody'] = 'a1_necroargpaul',
+    ['a1_necrobosmerbody'] = 'a1_necrobosmerpaul',
+    ['a1_necrobretonbody'] = 'a1_necrobretonpaul',
+    ['a1_necrodunmerbody'] = 'a1_necrodunmerpaul',
+    ['a1_necroimpbody'] = 'a1_necroimppaul',
+    ['a1_necrokhabody'] = 'a1_necrokhapaul',
+    ['a1_necronordbody'] = 'a1_necronordpaul',
+    ['a1_necroorcbody'] = 'a1_necroorcpaul',
+    ['a1_necrorgbody'] = 'a1_necrorgpaul',
+}
+
 return {
     engineHandlers = {
         onSave = function()
@@ -21,6 +60,45 @@ return {
         onLoad = function(data)
             if data then
                 heads = data
+            end
+        end,
+        onItemActive = function(item)
+            if types.Armor.objectIsInstance(item) then
+                local id = types.Armor.record(item).id
+                local activatorId = bodies[id]
+                if activatorId then
+                    local activator = world.createObject(activatorId)
+                    activator:teleport(item.cell, item.position, item.rotation)
+                    item:remove()
+                end
+            elseif types.Miscellaneous.objectIsInstance(item) then
+                local id = types.Miscellaneous.record(item).id
+                if string.sub(id, 1, #'a1_necroh') == 'a1_necroh' then
+                    local hash = string.sub(id, #'a1_necroh' + 1)
+                    local activatorId = 'a1_necrox' .. hash
+                    local activator = world.createObject(activatorId)
+                    activator:teleport(item.cell, item.position, item.rotation)
+                    item:remove()
+                end
+            end
+        end,
+        onActivate = function(object, actor)
+            if types.Activator.objectIsInstance(object) then
+                local id = types.Activator.record(object).id
+                if string.sub(id, 1, #'a1_necrox') == 'a1_necrox' then
+                    local hash = string.sub(id, #'a1_necrox' + 1)
+                    local headId = 'a1_necroh' .. hash
+                    local head = world.createObject(headId)
+                    head:moveInto(types.Actor.inventory(actor))
+                    object:remove()
+                else
+                    local paulId = paulsByBodies[id]
+                    if paulId then
+                        local paul = world.createObject(paulId)
+                        paul:moveInto(types.Actor.inventory(actor))
+                        object:remove()
+                    end
+                end
             end
         end,
     },
@@ -32,35 +110,13 @@ return {
                 item:moveInto(types.Actor.inventory(data.player))
             end
             local record = types.NPC.record(target)
-            local hair = world.createObject('A1_NecroH' .. idHash(record.hair))
+            local hair = world.createObject('a1_necroh' .. idHash(record.hair))
             hair:moveInto(types.Actor.inventory(data.player))
-            local head = world.createObject('A1_NecroH' .. idHash(record.head))
+            local head = world.createObject('a1_necroh' .. idHash(record.head))
             head:moveInto(types.Actor.inventory(data.player))
             heads[head.id] = record.id
-            local race = record.race
-            local body
-            if race == 'high elf' then
-                body = 'A1_NecroAltmerPaul'
-            elseif race == 'argonian' then
-                body = 'A1_NecroArgPaul'
-            elseif race == 'wood elf' then
-                body = 'A1_NecroBosmerPaul'
-            elseif race == 'breton' then
-                body = 'A1_NecroBretonPaul'
-            elseif race == 'dark elf' then
-                body = 'A1_NecroDunmerPaul'
-            elseif race == 'imperial' then
-                body = 'A1_NecroImpPaul'
-            elseif race == 'khajiit' then
-                body = 'A1_NecroKhaPaul'
-            elseif race == 'nord' then
-                body = 'A1_NecroNordPaul'
-            elseif race == 'orc' then
-                body = 'A1_NecroOrcPaul'
-            else
-                body = 'A1_NecroRgPaul'
-            end
-            local bodyObj = world.createObject(body, 1)
+            local body = pauls[record.race]
+            local bodyObj = world.createObject(body)
             bodyObj:moveInto(types.Actor.inventory(data.player))
             target:remove()
         end,
